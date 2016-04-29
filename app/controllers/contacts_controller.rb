a82fc8327441964e6f090b7a43d6dfec80fbc32b
jagadeesh <jagadeesh.k@techsophy.com>
2016-04-29 13:09:17 +0530

    Added ajax pagination

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,12 @@ class ContactsController < ApplicationController
 
   def index
     @contacts = Contact.all.page(params[:page]).per(10)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
+
   def new
     @contact = Contact.new
   end

01786bdf01b766c3abd4cdca94b05be953b7cd1c
jagadeesh <k.jagadeesh123@gmail.com>
2016-05-28 17:15:40 +0530

    Initial commit for adding inspania theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,12 @@ module ApplicationHelper
     title page_title
     add_normalized_breadcrumb(page_title)
   end
+
+  def is_active_controller(controller_name)
+    params[:controller] == controller_name ? "active" : nil
+  end
+
+  def is_active_action(action_name)
+    params[:action] == action_name ? "active" : nil
+  end
 end

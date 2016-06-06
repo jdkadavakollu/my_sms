3be31743e097dfae2f07453e2a6765f60044bedb
jagadeesh <k.jagadeesh123@gmail.com>
2016-06-06 23:37:11 +0530

    Template resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603084027) do
+ActiveRecord::Schema.define(version: 20160605121116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20160603084027) do
 
   add_index "contacts", ["group_id"], name: "index_contacts_on_group_id", using: :btree
 
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
+
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
@@ -35,11 +44,54 @@ ActiveRecord::Schema.define(version: 20160603084027) do
 
   add_index "groups", ["user_id"], name: "index_groups_on_user_id", using: :btree
 
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sms_group_id"
+    t.integer  "contact_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "messages", ["contact_id"], name: "index_messages_on_contact_id", using: :btree
+  add_index "messages", ["sms_group_id"], name: "index_messages_on_sms_group_id", using: :btree
+
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
+
+  create_table "sms_groups", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sms_groups", ["user_id"], name: "index_sms_groups_on_user_id", using: :btree
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "fname"
+    t.string   "mobile"
+    t.date     "dob"
+    t.text     "address"
+    t.integer  "user_id"
+    t.integer  "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "students", ["section_id"], name: "index_students_on_section_id", using: :btree
+  add_index "students", ["user_id"], name: "index_students_on_user_id", using: :btree
 
   create_table "template_items", force: :cascade do |t|
     t.string   "name"
@@ -80,6 +132,13 @@ ActiveRecord::Schema.define(version: 20160603084027) do
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
   add_foreign_key "contacts", "groups"
+  add_foreign_key "courses", "users"
   add_foreign_key "groups", "users"
+  add_foreign_key "messages", "contacts"
+  add_foreign_key "messages", "sms_groups"
+  add_foreign_key "sections", "courses"
+  add_foreign_key "sms_groups", "users"
+  add_foreign_key "students", "sections"
+  add_foreign_key "students", "users"
   add_foreign_key "templates", "template_items"
 end

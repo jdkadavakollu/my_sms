2915d7674da94f865d51bfc9e08ea780fde2d6b9
jagadeesh <k.jagadeesh123@gmail.com>
2016-07-12 12:39:11 +0530

    Removed inspania theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,4 @@
 //= require jquery/jquery-2.1.1.js
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require metisMenu/jquery.metisMenu.js
-//= require pace/pace.min.js
-//= require slimscroll/jquery.slimscroll.min.js
 //= require_tree .

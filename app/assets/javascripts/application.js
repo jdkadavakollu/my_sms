4f64d7f0e88c090db389a7ef286edc5233681ede
jagadeesh <k.jagadeesh123@gmail.com>
2016-07-30 23:47:51 +0530

    Added Gentella theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,15 +2,44 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
+// compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
 //
-//= require jquery/jquery-2.1.1.js
-//= require jquery_ujs
+//= require jquery
 //= require bootstrap-sprockets
-//= require_tree .
+
+//= require jquery_ujs
+//= require turbolinks
+
+//= require js/flot/jquery.flot.js
+//= require js/flot/jquery.flot.pie.js
+//= require js/flot/jquery.flot.orderBars.js
+//= require js/flot/jquery.flot.time.min.js
+//= require js/flot/date.js
+//= require js/flot/jquery.flot.spline.js
+//= require js/flot/jquery.flot.stack.js
+//= require js/flot/curvedLines.js
+//= require js/flot/jquery.flot.resize.js
+
+//= require js/progressbar/bootstrap-progressbar.min.js
+
+//= require js/icheck/icheck.min.js
+
+//= require js/moment/moment.min.js
+//= require js/datepicker/daterangepicker.js
+
+//= require js/chartjs/chart.min.js
+
+//= require js/pace/pace.min.js
+
+//= require  js/nprogress.js
+
+//= require flash.js
+
+
+

ead7d23fd8001945cbd2d2031259c50da112fa88
jagadeesh <k.jagadeesh123@gmail.com>
2016-09-15 21:39:23 +0530

    Removed unused files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,19 +25,6 @@
 //= require js/flot/jquery.flot.stack.js
 //= require js/flot/curvedLines.js
 //= require js/flot/jquery.flot.resize.js
-
 //= require js/progressbar/bootstrap-progressbar.min.js
-
-//= require js/icheck/icheck.min.js
-
-//= require js/moment/moment.min.js
-//= require js/datepicker/daterangepicker.js
-
-//= require js/chartjs/chart.min.js
-
 //= require js/pace/pace.min.js
-
 //= require flash.js
-
-
-
